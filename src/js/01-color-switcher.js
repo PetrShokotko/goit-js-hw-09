5b66cee87312594fa774b056cf0456b97e14740a
PetrShokotko <shokotkop@gmail.com>
2023-07-27 14:07:11 +0300

    Update first

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,30 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, '0')}`;
+}
 
+const startButton = document.querySelector('[data-start]');
+const stopButton = document.querySelector('[data-stop]');
+const body = document.body;
+
+let intervalId = null;
+
+function setColorInterval() {
+  intervalId = setInterval(() => {
+    body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+}
+
+function startColorSwitcher() {
+  startButton.disabled = true;
+  stopButton.disabled = false;
+  setColorInterval();
+}
+
+function stopColorSwitcher() {
+  startButton.disabled = false;
+  stopButton.disabled = true;
+  clearInterval(intervalId);
+}
+
+startButton.addEventListener('click', startColorSwitcher);
+stopButton.addEventListener('click', stopColorSwitcher);
