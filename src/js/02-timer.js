5cd41eae10b37df2ec30df419b998136c60286e4
PetrShokotko <shokotkop@gmail.com>
2023-07-29 13:27:29 +0300

    Update

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,4 +1,3 @@
-
 import flatpickr from "flatpickr";
 import "flatpickr/dist/flatpickr.min.css";
 
@@ -27,6 +26,8 @@ const hoursElement = document.querySelector('[data-hours]');
 const minutesElement = document.querySelector('[data-minutes]');
 const secondsElement = document.querySelector('[data-seconds]');
 
+let selectedDate;
+let countdownInterval;
 
 const options = {
   enableTime: true,
@@ -34,8 +35,7 @@ const options = {
   defaultDate: new Date(),
   minuteIncrement: 1,
   onClose(selectedDates) {
-    const selectedDate = selectedDates[0];
-
+    selectedDate = selectedDates[0];
 
     if (selectedDate < new Date()) {
       window.alert("Пожалуйста, укажите дату в будущем");
@@ -48,28 +48,104 @@ const options = {
 
 flatpickr("#datetime-picker", options);
 
-let countdownInterval;
+function updateCountdown() {
+  const currentDate = new Date().getTime();
+  const timeLeft = selectedDate - currentDate;
+
+  if (timeLeft <= 0) {
+    clearInterval(countdownInterval);
+    daysElement.textContent = '00';
+    hoursElement.textContent = '00';
+    minutesElement.textContent = '00';
+    secondsElement.textContent = '00';
+    datetimePicker.disabled = false;
+  } else {
+    const { days, hours, minutes, seconds } = convertMs(timeLeft);
+    daysElement.textContent = addLeadingZero(days);
+    hoursElement.textContent = addLeadingZero(hours);
+    minutesElement.textContent = addLeadingZero(minutes);
+    secondsElement.textContent = addLeadingZero(seconds);
+  }
+}
 
 startButton.addEventListener('click', () => {
-
-  const selectedDate = new Date(datetimePicker.value).getTime();
-
-  countdownInterval = setInterval(() => {
-    const currentDate = new Date().getTime();
-    const timeLeft = selectedDate - currentDate;
-
-    if (timeLeft <= 0) {
-      clearInterval(countdownInterval);
-      daysElement.textContent = '00';
-      hoursElement.textContent = '00';
-      minutesElement.textContent = '00';
-      secondsElement.textContent = '00';
-    } else {
-      const { days, hours, minutes, seconds } = convertMs(timeLeft);
-      daysElement.textContent = addLeadingZero(days);
-      hoursElement.textContent = addLeadingZero(hours);
-      minutesElement.textContent = addLeadingZero(minutes);
-      secondsElement.textContent = addLeadingZero(seconds);
-    }
-  }, 1000);
+  datetimePicker.disabled = true;
+  clearInterval(countdownInterval);
+  countdownInterval = setInterval(updateCountdown, 1000);
+  updateCountdown();
 });
+
+// import flatpickr from "flatpickr";
+// import "flatpickr/dist/flatpickr.min.css";
+
+// function addLeadingZero(value) {
+//   return value.toString().padStart(2, '0');
+// }
+
+// function convertMs(ms) {
+//   const second = 1000;
+//   const minute = second * 60;
+//   const hour = minute * 60;
+//   const day = hour * 24;
+
+//   const days = Math.floor(ms / day);
+//   const hours = Math.floor((ms % day) / hour);
+//   const minutes = Math.floor(((ms % day) % hour) / minute);
+//   const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+//   return { days, hours, minutes, seconds };
+// }
+
+// const datetimePicker = document.getElementById('datetime-picker');
+// const startButton = document.querySelector('[data-start]');
+// const daysElement = document.querySelector('[data-days]');
+// const hoursElement = document.querySelector('[data-hours]');
+// const minutesElement = document.querySelector('[data-minutes]');
+// const secondsElement = document.querySelector('[data-seconds]');
+
+
+// const options = {
+//   enableTime: true,
+//   time_24hr: true,
+//   defaultDate: new Date(),
+//   minuteIncrement: 1,
+//   onClose(selectedDates) {
+//     const selectedDate = selectedDates[0];
+
+
+//     if (selectedDate < new Date()) {
+//       window.alert("Пожалуйста, укажите дату в будущем");
+//       startButton.disabled = true;
+//     } else {
+//       startButton.disabled = false;
+//     }
+//   },
+// };
+
+// flatpickr("#datetime-picker", options);
+
+// let countdownInterval;
+
+// startButton.addEventListener('click', () => {
+
+//   const selectedDate = new Date(datetimePicker.value).getTime();
+
+//   countdownInterval = setInterval(() => {
+//     const currentDate = new Date().getTime();
+//     const timeLeft = selectedDate - currentDate;
+
+//     if (timeLeft <= 0) {
+//       clearInterval(countdownInterval);
+//       daysElement.textContent = '00';
+//       hoursElement.textContent = '00';
+//       minutesElement.textContent = '00';
+//       secondsElement.textContent = '00';
+//     } else {
+//       const { days, hours, minutes, seconds } = convertMs(timeLeft);
+//       daysElement.textContent = addLeadingZero(days);
+//       hoursElement.textContent = addLeadingZero(hours);
+//       minutesElement.textContent = addLeadingZero(minutes);
+//       secondsElement.textContent = addLeadingZero(seconds);
+//     }
+//   }, 1000);
+// });
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,12 +1,3 @@
-// function createPromise(position, delay) {
-//   const shouldResolve = Math.random() > 0.3;
-//   if (shouldResolve) {
-//     // Fulfill
-//   } else {
-//     // Reject
-//   }
-// }
-
 function createPromise(position, delay) {
   return new Promise((resolve, reject) => {
     const shouldResolve = Math.random() > 0.3;
@@ -29,6 +20,11 @@ document.addEventListener("DOMContentLoaded", () => {
     const step = parseInt(formData.get("step"), 10);
     const amount = parseInt(formData.get("amount"), 10);
 
+    if (step < 0 || firstDelay < 0 || amount <= 0) {
+      alert("Шаг задержки или количество содержат недопустимые значения.");
+      return;
+    }
+
     for (let i = 0; i < amount; i++) {
       const position = i + 1;
       const delay = firstDelay + i * step;
@@ -42,3 +38,49 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+
+
+// function createPromise(position, delay) {
+//   const shouldResolve = Math.random() > 0.3;
+//   if (shouldResolve) {
+//     // Fulfill
+//   } else {
+//     // Reject
+//   }
+// }
+
+// function createPromise(position, delay) {
+//   return new Promise((resolve, reject) => {
+//     const shouldResolve = Math.random() > 0.3;
+//     setTimeout(() => {
+//       if (shouldResolve) {
+//         resolve({ position, delay });
+//       } else {
+//         reject({ position, delay });
+//       }
+//     }, delay);
+//   });
+// }
+
+// document.addEventListener("DOMContentLoaded", () => {
+//   const form = document.querySelector(".form");
+//   form.addEventListener("submit", (event) => {
+//     event.preventDefault();
+//     const formData = new FormData(form);
+//     const firstDelay = parseInt(formData.get("delay"), 10);
+//     const step = parseInt(formData.get("step"), 10);
+//     const amount = parseInt(formData.get("amount"), 10);
+
+//     for (let i = 0; i < amount; i++) {
+//       const position = i + 1;
+//       const delay = firstDelay + i * step;
+//       createPromise(position, delay)
+//         .then(({ position, delay }) => {
+//           console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+//         })
+//         .catch(({ position, delay }) => {
+//           console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+//         });
+//     }
+//   });
+// });
